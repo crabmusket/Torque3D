bd3d907f06f70ab831860223e3fe91211485e34a
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-05-30 10:45:58 +1000

    Re-ordered dependencies and removed explicit git version.

--- vm/provision.sh
+++ vm/provision.sh
@@ -2,11 +2,11 @@ apt-get update -y > /dev/null
 
 # Install prerequisites
 apt-get install \
+	git \
+	cmake=2.8.7-0ubuntu5 \
 	build-essential=11.5ubuntu2.1 \
 	nasm=2.09.10-1 \
-	git=1:1.7.9.5-1 \
 	php5-cli=5.3.10-1ubuntu3.11 \
 	libsdl-dev \
 	libogg-dev \
-	cmake=2.8.7-0ubuntu5 \
 	-y
